73c820a054035fccda8b706c1ce36a9b0df26d2f
Isha Jaswal <iamishajaswal@gmail.com>
2025-09-30 09:54:15 +0530

    fix: update ALLOWED_HOSTS for Render

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -37,7 +37,11 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "dynamic-portfolio-n50q.onrender.com",
+    "localhost",
+    "127.0.0.1"
+]
 
 
 # Application definition
@@ -97,12 +101,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("ENGINE", "django.db.backends.mysql"),
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST", "127.0.0.1"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'ENGINE': os.getenv("ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.getenv("DB_NAME",  BASE_DIR / "db.sqlite3"),
+        'USER': os.getenv("DB_USER",""),
+        'PASSWORD': os.getenv("DB_PASSWORD",""),
+        'HOST': os.getenv("DB_HOST", ""),
+        'PORT': os.getenv("DB_PORT", ""),
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
