176e218f1a3378ba13c7c7f19e9f4d103902511a
Isha Jaswal <iamishajaswal@gmail.com>
2025-09-30 10:02:50 +0530

    fix: add STATIC_ROOT for render

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -154,6 +154,8 @@ STATICFILES_DIRS = [
     os.path.join(REACT_BUILD_DIR, 'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
