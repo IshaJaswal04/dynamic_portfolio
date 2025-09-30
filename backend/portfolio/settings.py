30de1b5197ad4826c233a4069590bc65f73d69a9
Isha Jaswal <iamishajaswal@gmail.com>
2025-09-30 10:08:34 +0530

    fix : use SQLite for deployment

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': os.getenv("ENGINE", "django.db.backends.sqlite3"),
         'NAME': os.getenv("DB_NAME",  BASE_DIR / "db.sqlite3"),
@@ -110,6 +110,13 @@ DATABASES = {
         "OPTIONS": {
             "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
         },
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
