f23192d3fe968e15d87a56b3eccabbc8748c1225
Isha Jaswal <iamishajaswal@gmail.com>
2025-10-02 16:51:44 +0530

    Fix sttings and prpare for Render deployment

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
+import dj_database_url
 
 
 load_dotenv()
@@ -98,12 +99,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv("ENGINE"),
+        'ENGINE': os.getenv("ENGINE", "django.db.backends.postgresql"),
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'PORT': os.getenv("DB_PORT"),
         
     }
 }
