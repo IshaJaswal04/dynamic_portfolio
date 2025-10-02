a7d275d0e5fe38aea22b2a318f299f1925cb066e
Isha Jaswal <iamishajaswal@gmail.com>
2025-10-02 16:35:45 +0530

    Fixing syntax

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["dynamic-portfolio-n50q.onrender.com"]
 
 
 # Application definition
@@ -56,9 +56,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -68,7 +69,7 @@ MIDDLEWARE = [
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "https://dynamic-portfolio-frontend-iota.vercel.app",
 ]
 
 ROOT_URLCONF = 'portfolio.urls'
@@ -77,7 +78,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [REACT_BUILD_DIR],  # React index.html location
+        'DIRS': [],  # React index.html location
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,9 +145,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(REACT_BUILD_DIR, 'static')
-]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
