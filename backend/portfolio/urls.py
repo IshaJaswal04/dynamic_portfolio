a57278b31aa7a87929588906a084a6bea1b51dc1
Isha Jaswal <iamishajaswal@gmail.com>
2025-10-03 11:06:33 +0530

    Fixing urls

--- backend/portfolio/urls.py
+++ backend/portfolio/urls.py
@@ -31,6 +31,9 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 # Keep catch-all LAST
 urlpatterns += [re_path(r'^.*$', views.index)]
