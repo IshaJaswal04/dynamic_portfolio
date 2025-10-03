8c79354ffdfac4593319b7eb359561144e520b1d
Isha Jaswal <iamishajaswal@gmail.com>
2025-10-03 11:18:06 +0530

    fix: absolute urls

--- backend/content/serializers.py
+++ backend/content/serializers.py
@@ -1,11 +1,29 @@
 from rest_framework import serializers
-from .models import Navbar, Footer, Home, About, Skill, Project, ContactInfo, Certificate, Experience, Contact, Contactform, AboutHero, Education, CTA, ContactHero, ContactDetails, SkillHero, Formdetail, Softskills, Logo
+from .models import (
+    Navbar, Footer, Home, About, Skill, Project, ContactInfo, Certificate,
+    Experience, Contact, Contactform, AboutHero, Education, CTA, ContactHero,
+    ContactDetails, SkillHero, Formdetail, Softskills, Logo
+)
+
+# ✅ Helper Mixin for Image URLs
+class AbsoluteImageUrlMixin:
+    def get_image_url(self, obj, field_name):
+        request = self.context.get("request")
+        field = getattr(obj, field_name)
+        if field:
+            return request.build_absolute_uri(field.url)
+        return None
 
 
-class LogoSerializers(serializers.ModelSerializer):
+class LogoSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Logo
         fields = "__all__"
+
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
 
 
 class SoftskillsSerializers(serializers.ModelSerializer):
@@ -14,24 +32,33 @@ class SoftskillsSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
-
 class FormdetailSerializers(serializers.ModelSerializer):
     class Meta:
         model = Formdetail
         fields = '__all__'
 
 
-class SkillHeroSerializers(serializers.ModelSerializer):
+class SkillHeroSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = SkillHero
         fields = '__all__'
 
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
 
-class ContactHeroSerializers(serializers.ModelSerializer):
+
+class ContactHeroSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = ContactHero
         fields = '__all__'
+
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
+
 
 class ContactDetailsSerializers(serializers.ModelSerializer):
     class Meta:
@@ -39,12 +66,15 @@ class ContactDetailsSerializers(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class CTASerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
 
-
-class CTASerializers(serializers.ModelSerializer):
     class Meta:
         model = CTA
         fields = '__all__'
+
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
 
 
 class EducationSerializers(serializers.ModelSerializer):
@@ -52,10 +82,16 @@ class EducationSerializers(serializers.ModelSerializer):
         model = Education
         fields = '__all__'
 
-class AboutHeroSerializers(serializers.ModelSerializer):
+
+class AboutHeroSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = AboutHero
         fields = '__all__'
+
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
 
 
 class ContactformSerializers(serializers.ModelSerializer):
@@ -63,54 +99,97 @@ class ContactformSerializers(serializers.ModelSerializer):
         model = Contactform
         fields = '__all__'
 
+
 class ContactSerializers(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields = '__all__'  
+        fields = '__all__'
+
 
 class NavbarSerializers(serializers.ModelSerializer):
     class Meta:
         model = Navbar
         fields = '__all__'
 
-class FooterSerializers(serializers.ModelSerializer):
-    icon = serializers.ImageField(use_url=True) 
+
+class FooterSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    icon = serializers.SerializerMethodField()
+
     class Meta:
         model = Footer
         fields = '__all__'
 
-class HomeSerializers(serializers.ModelSerializer):
+    def get_icon(self, obj):
+        return self.get_image_url(obj, "icon")
+
+
+class HomeSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Home
         fields = '__all__'
 
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
 
-class AboutSerializers(serializers.ModelSerializer):
+
+class AboutSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = About
         fields = '__all__'
 
-class SkillSerializers(serializers.ModelSerializer):
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
+
+
+class SkillSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Skill
         fields = '__all__'
 
-class ProjectSerializers(serializers.ModelSerializer):
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
+
+
+class ProjectSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Project
         fields = '__all__'
+
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
+
 
 class ContactInfoSerializers(serializers.ModelSerializer):
     class Meta:
         model = ContactInfo
         fields = '__all__'
 
-class CertificateSerializers(serializers.ModelSerializer):
+
+class CertificateSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Certificate
         fields = '__all__'
 
-class ExperienceSerializers(serializers.ModelSerializer):
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
+
+
+class ExperienceSerializers(serializers.ModelSerializer, AbsoluteImageUrlMixin):
+    image = serializers.SerializerMethodField()
+
     class Meta:
         model = Experience
         fields = '__all__'
+
+    def get_image(self, obj):
+        return self.get_image_url(obj, "image")
