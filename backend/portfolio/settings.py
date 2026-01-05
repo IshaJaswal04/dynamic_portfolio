fe4822bcd6069a40468c7c53b3a24a0b3da0dcd7
Isha Jaswal <iamishajaswal@gmail.com>
2026-01-05 18:33:41 +0530

    Fixing database

--- backend/portfolio/settings.py
+++ backend/portfolio/settings.py
@@ -100,15 +100,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.getenv("ENGINE", "django.db.backends.postgresql"),
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST","127.0.0.1:8000"),
-        'PORT': os.getenv("DB_PORT"),
-        
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+
 }
 
 
